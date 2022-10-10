de97075838929b6022ed23a7081c343782a0189d
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-10-11 01:23:07 +0700

    fix register, login

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -97,12 +97,8 @@
                     <div class="mb-3">
                       <input type="password" class="form-control" id="password" name="password" placeholder="Password" aria-label="Password" aria-describedby="password-addon" required>
                     </div>
-                    <!-- <div class="form-check form-switch">
-                      <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
-                      <label class="form-check-label" for="rememberMe">Remember me</label>
-                    </div> -->
                     <div class="text-center">
-                      <button type="button" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+                      <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
                     </div>
                   </form>
                 </div>
