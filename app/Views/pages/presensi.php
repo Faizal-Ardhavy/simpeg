6514f26dfad6b860842e835cf1feca76e487af32
karinadityas <karina.adityasramadhanti2041@students.unila.ac.id>
2022-10-31 22:54:05 +0700

    delete name

--- app/Views/pages/presensi.php
+++ app/Views/pages/presensi.php
@@ -7,7 +7,6 @@
             <thead class="table-light">
                 <tr>
                     <th>ID</th>
-                    <th>Nama</th>
                     <th>Tanggal</th>
                     <th>Jam</th>
                     <th>Keterangan</th>
@@ -16,28 +15,24 @@
             <tbody class="table-border-bottom-0">
                 <tr>
                     <td>001</td>
-                    <td>Kayla Atsila Ivanka</td>
                     <td>11-01-2022</td>
                     <td>07:01</td>
                     <td>Hadir</td>
                 </tr>
                 <tr>
                     <td>003</td>
-                    <td>Naufal Anbial Falah</td>
                     <td>11-01-2022</td>
                     <td>06:40</td>
                     <td>Hadir</td>
                 </tr>
                 <tr>
                     <td>002</td>
-                    <td>Karina Adityas Ramadhanti</td>
                     <td>11-01-2022</td>
                     <td>07:12</td>
                     <td>Hadir</td>
                 </tr>
                 <tr>
                     <td>004</td>
-                    <td>Muhammad Faizal Ardhavy Heru</td>
                     <td>11-01-2022</td>
                     <td>07:30</td>
                     <td>Hadir</td>
