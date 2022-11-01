47acd018a8d76bc677b447c7505d9a265515680d
Faizal-Ardhavy <79374894+Faizal-Ardhavy@users.noreply.github.com>
2022-11-01 08:31:36 +0700

    view

--- app/Views/pages/presensi.php
+++ app/Views/pages/presensi.php
@@ -38,38 +38,6 @@
             </div>
         </div>
     </div>
-=======
-	<div class="row">
-		<div class="col-12">
-			<div class="card h-100">
-				<div class="card-header pb-0">
-					<div class="row">
-						<div class="col-md-8 d-flex align-items-center">
-							<h6 class="mb-0 mx-2">Riwayat Presensi</h6>
-						</div>
-					</div>
-				</div>
-				<div class="card-body">
-					<hr class="horizontal gray-light" style="margin: 0 !important;">
-					<table class="table text-dark ps-0 text-sm">
-						<tr>
-							<th style="width: 10% !important;">ID</th>
-							<th style="width: 25% !important;">Tanggal</th>
-							<th style="width: 25% !important;">Jam</th>
-							<th style="width: 25% !important;">Keterangan</th>
-						</tr>
-						<tr>
-							<td style="width: 10% !important;">001</td>
-							<td style="width: 25% !important;">10-31-2022</td>
-							<td style="width: 25% !important;">08:33</td>
-							<td style="width: 25% !important;">Hadir</td>
-						</tr>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
->>>>>>> 4d306d06aa588da8d6d71fc46a6db7519bf4748c
 </div>
 
 <?= $this->endSection() ?>
