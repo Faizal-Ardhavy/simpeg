948b6e783c5b8303ba4dd066356f3cfe5a703c31
kaylaatsila <kaylaatsilaivanka@gmail.com>
2022-11-21 19:18:10 +0700

    add stater file for cetak laporan

---
+++ app/Views/pages/laporan/cetak.php
@@ -0,0 +1,73 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="/assets/img/favicon.png">
+	<title>
+		SIMPEG - Cetak
+	</title>
+	<!--     Fonts     -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+	<!-- CSS Files -->
+	<link id="pagestyle" href="/assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+	<link href="/assets/css/laporan.css" rel="stylesheet" />
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="row g-4">
+			<div class="col">
+				<div class="rounded h-100">
+					<header>
+						<div class="d-flex align-items-end flex-column">
+							<div class="">
+								<button class="btn btn-dark my-3" id="printButton" onClick="window.print();">
+									<i class="bi bi-printer"></i> Cetak</button>
+							</div>
+						</div>
+						<div class="d-flex justify-content-between">
+							<div class="text-center">
+
+							</div>
+						</div>
+						<hr>
+					</header>
+					<h6 class="mt-5 text-uppercase"><b>Data Diri Pegawai</b></h6>
+					<table class="table table-borderless table-responsive">
+						<thead>
+							<tr>
+
+							</tr>
+						</thead>
+
+						<tbody>
+							<tr>
+
+							</tr>
+						</tbody>
+
+					</table>
+					<h6 class="mt-5 text-uppercase"><b>Rekapitulasi Pegawai</b></h6>
+					<table class="table table-borderless table-responsive align-middle">
+						<thead>
+							<tr>
+
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
